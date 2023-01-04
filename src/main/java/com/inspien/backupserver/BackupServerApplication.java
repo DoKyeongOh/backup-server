699d95bac272b50e5022430822aa4f1a901c3487
DoKyeong Oh <dkproh@gmail.com>
2023-01-04 13:50:38 +0900

    feat : 스프링 부트 + 메이븐 프로젝트 생성

---
+++ src/main/java/com/inspien/backupserver/BackupServerApplication.java
@@ -0,0 +1,13 @@
+package com.inspien.backupserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackupServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackupServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inspien/backupserver/BackupServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inspien.backupserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackupServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
